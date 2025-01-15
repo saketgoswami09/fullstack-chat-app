4eb890e8e443302d4c44291ac6fab03894f6c9d3
saket <saketgoswami4141@gmail.com>
2025-01-15 21:35:40 +0530

    deploy issue fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,11 +31,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
+  // Update this line to correctly serve from the frontend/dist folder
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
+
 
 server.listen(PORT, () => {
   console.log("Server is running on PORT: " + PORT);
