091c4821bc2afd5ed48995d47c33b8eded40e030
saket <saketgoswami4141@gmail.com>
2025-01-15 23:31:20 +0530

    Initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
-import { fileURLToPath } from "url";  // Importing for ES module compatibility
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -13,10 +14,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-
-// Define __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -31,7 +29,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  // Update this line to correctly serve from the frontend/dist folder
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
@@ -39,8 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
 server.listen(PORT, () => {
-  console.log("Server is running on PORT: " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
