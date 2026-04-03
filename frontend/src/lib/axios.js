ecc4ba87670268e56f332fa32409e6d5ae11e6de
Saket goswami <110333651+saketgoswami09@users.noreply.github.com>
2026-04-03 15:16:55 +0530

    Update axios baseURL to production API endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://fullstack-chat-app-2-ynz0.onrender.com/api",
   withCredentials: true,
 });
