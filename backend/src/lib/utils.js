39524f3e474881d35b8d0b63a227f3ec6b8168d3
Saket goswami <110333651+saketgoswami09@users.noreply.github.com>
2026-04-03 15:29:51 +0530

    Adjust cookie settings based on environment

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,12 +5,15 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
+const isProduction = process.env.NODE_ENV === "production";
+
 res.cookie("jwt", token, {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "none",     
-  secure: true,         
+  sameSite: isProduction ? "none" : "lax",
+  secure: isProduction,
 });
+
 
 
   return token;
